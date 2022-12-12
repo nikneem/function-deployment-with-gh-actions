b9f4598b02e73f526a0850a17dc7751ed0b5909e
Eduard <e.keilholz@4dotnet.nl>
2022-12-12 13:17:40 +0100

    Linux worker

--- src/Demo.FunctionsApp/Functions/DemoFunction.cs
+++ src/Demo.FunctionsApp/Functions/DemoFunction.cs
@@ -17,8 +17,6 @@ namespace Demo.FunctionsApp.Functions
         [Function("DemoFunction")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
             response.WriteString("Demo function works!");
