89c6e21e537a09b6e0d4733708282364e1c66d7f
Eduard <e.keilholz@4dotnet.nl>
2022-12-12 09:50:15 +0100

    Publish first project

---
+++ src/Demo.FunctionsApp/Functions/DemoFunction.cs
@@ -0,0 +1,29 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.FunctionsApp.Functions
+{
+    public class DemoFunction
+    {
+        private readonly ILogger _logger;
+
+        public DemoFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<DemoFunction>();
+        }
+
+        [Function("DemoFunction")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString("Demo function works!");
+
+            return response;
+        }
+    }
+}
